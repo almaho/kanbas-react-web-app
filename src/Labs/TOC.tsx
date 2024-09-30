366c2d1c3fd3fb2a2a53f39c1ed79554a8e17c96
almaho <126069668+almaho@users.noreply.github.com>
2024-09-30 04:04:10 +0000

    a1

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
 
 export default function TOC() {
-    return (
-      <ul>
-        <li><Link to="/Labs">Labs</Link></li>
-        <li><Link to="/Labs/Lab1">Lab 1</Link></li>
-        <li><Link to="/Labs/Lab2">Lab 2</Link></li>
-        <li><Link to="/Labs/Lab3">Lab 3</Link></li>
-        <li><Link to="/Kanbas">Kanbas</Link></li>
-      </ul>
-    );
-  }
-  
+  return (
+    <ul>
+      <li><Link to="/Labs">Labs</Link></li>
+      <li><Link to="/Labs/Lab1">Lab 1</Link></li>
+      <li><Link to="/Labs/Lab2">Lab 2</Link></li>
+      <li><Link to="/Labs/Lab3">Lab 3</Link></li>
+      <li><Link to="/Kanbas">Kanbas</Link></li>
+      <li>
+        <a 
+          href="https://github.com/almaho/kanbas-react-web-app" 
+          id="wd-github" 
+          target="_blank" 
+          rel="noreferrer"
+        >
+          GitHub Repository
+        </a>
+      </li>
+    </ul>
+  );
+}
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,16 +1,14 @@
-
 import { Route, Routes, Navigate } from "react-router";
 import Account from "../Kanbas/Account";
-
 
 export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
+      <h2>Alma Ghafari</h2> 
       <Routes>
-      <Route path="/" element={<Navigate to="Account" />} />
-      <Route path="/Account/*" element={<Account />} />
-
+        <Route path="/" element={<Navigate to="Account" />} />
+        <Route path="/Account/*" element={<Account />} />
       </Routes>
     </div>
   );

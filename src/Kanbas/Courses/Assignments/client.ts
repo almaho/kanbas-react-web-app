6f3f947503f022f338b3b75d4a7b82bbfa90ab2d
almaho <126069668+almaho@users.noreply.github.com>
2024-11-28 17:11:25 -0500

    Update client.ts

--- src/Kanbas/Courses/Assignments/client.ts
+++ src/Kanbas/Courses/Assignments/client.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+axios.defaults.withCredentials = true;
 
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const ASSIGNMENTS_API = `${REMOTE_SERVER}/api/assignments`;
